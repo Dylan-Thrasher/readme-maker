98d571db2c2b2f08a3aae949eba7d00bb2f3e692
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-04-24 23:32:39 -0400

    Updated switch statements to work with results of license chosen

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# readme-maker
-A tool for creating a professional readme template
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+  # test
+  ## Description
+  test
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  ## Installation
+  test
+  ## Usage
+  test
+  ## License
+  MIT
+  ## Contributing
+  test
+  ## Tests 
+  test
+  ## Questions
+  undefined
+
+  ## License 
+  This project is licensed under the MIT license.
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,47 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const choice = (license);
+  switch (choice) {
+    case 'MIT':
+      return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`
+      break;
+
+    case "ISC":
+      return `![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]`
+      break;
+    
+    case "Mozilla":
+      return `![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)]`
+      break;
+  
+    default:
+      return ``;
+      break;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  return `[license](URL)`
+  const choice = (license);
+  switch (choice) {
+    case 'MIT':
+      return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+      break;
+
+    case "ISC":
+      return `[License: ISC](https://opensource.org/licenses/ISC)`
+      break;
+    
+    case "Mozilla":
+      return `[License: MPL 2.0](https://opensource.org/licenses/MPL-2.0)`
+      break;
+  
+    default:
+      return ``;
+      break;
+  }
 }
 
 // TODO: Create a function that returns the license section of README
@@ -28,7 +64,7 @@ function generateMarkdown(data) {
   * [License](#license)
   * [Contributing](#contributing)
   * [Tests](#tests)
-  *[Questions](#questions)
+  * [Questions](#questions)
   ## Installation
   ${data.installation}
   ## Usage
@@ -40,8 +76,9 @@ function generateMarkdown(data) {
   ## Tests 
   ${data.tests}
   ## Questions
-  ${data.questions}
+  For any questions, please reference my repositories at github.com/${data.github} or email me at ${data.email}
 ${renderLicenseSection(data.license)}
+${renderLicenseLink(data.license)}
 `;
 }
 
